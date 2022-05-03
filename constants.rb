6008634c61137c795fc4e4b88f83a26d3544d4cd
YoussefWilliam <youssef.william997@gmail.com>
2022-05-03 13:17:36 +0200

    learn methods on objects intro

---
+++ constants.rb
@@ -0,0 +1,12 @@
+age = 24
+p age
+
+age = 26
+p age
+
+NAME = "joey"
+p NAME
+
+# Error
+NAME = "willy" # =>already initialized constant NAME
+p NAME
---
+++ object_methods.rb
@@ -0,0 +1,22 @@
+name = 'My name is joe'
+age = 24
+
+# Length
+name_length = name.length
+
+# capital case
+name_upcase = name.upcase
+
+# lowe case
+name_lowecase = name.downcase
+
+# next number
+next_number = age.next
+
+p name_length
+p name_upcase
+p name_lowecase
+
+puts
+
+p next_number
